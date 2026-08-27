7da48c0b8af276e2f5555d438e8acbf5e84d6d8c
Danny Phillips <dannyphillips@Dannys-Mac-mini.local>
2026-08-28 00:35:22 +1000

    first commit

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+resource "aws_s3_bucket" "this" {
+  bucket = "my-website-bucket-project65"
+}
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+resource "aws_s3_bucket_policy" "public_read" {
+    bucket = aws_s3_bucket.this.id
+
+    depends_on = [
+        aws_s3_bucket_public_access_block.this
+    ]
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Sid       = "PublicReadGetObject"
+                Effect    = "Allow"
+                Principal = "*"
+                Action    = "s3:GetObject"
+                Resource  = "${aws_s3_bucket.this.arn}/*"
+            }
+        ]
+    })
+}
