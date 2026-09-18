915297705e65778c1536cd50b95b7bd2b9195d51
Milkmanlamp <dany.phillips@pm.me>
2026-09-18 18:15:41 +1000

    fixes bucket policy to link to the distros terrafrom recource instead of hardcoding

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_ownership_controls" "this" {
 
 
 resource "aws_s3_bucket_public_access_block" "this" {
-  bucket = aws_s3_bucket.this.id
+  bucket                  = aws_s3_bucket.this.id
   block_public_acls       = true
   ignore_public_acls      = true
   block_public_policy     = true
@@ -30,30 +30,30 @@ resource "aws_s3_bucket_website_configuration" "this" {
   }
 }
 resource "aws_s3_bucket_policy" "public_read" {
-    bucket = aws_s3_bucket.this.id
+  bucket = aws_s3_bucket.this.id
 
-    depends_on = [
-        aws_s3_bucket_public_access_block.this
+  depends_on = [
+    aws_s3_bucket_public_access_block.this
+  ]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.this.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.this.arn
+          }
+        }
+      }
     ]
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Sid       = "AllowCloudFrontServicePrincipalReadOnly"
-                Effect    = "Allow"
-                Principal = {
-                  Service = "cloudfront.amazonaws.com"
-                }
-                Action    = "s3:GetObject"
-                Resource  = "${aws_s3_bucket.this.arn}/*"
-                Condition = {
-                  StringEquals = {
-                    "AWS:SourceArn" = "arn:aws:cloudfront::665511041793:distribution/E1UB90KFJEPZOE"
-                  }
-                }
-            }
-        ]
-    })
+  })
 }
 
 locals {
@@ -87,45 +87,45 @@ resource "aws_s3_object" "website_files" {
 }
 ## Cloudfront
 resource "aws_cloudfront_origin_access_control" "this" {
-  name = "${aws_s3_bucket.this.id}-oac"
+  name        = "${aws_s3_bucket.this.id}-oac"
   description = "allow access into the bucket from cloudfront"
 
   origin_access_control_origin_type = "s3"
-  signing_behavior = "always"
-  signing_protocol = "sigv4"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 resource "aws_cloudfront_distribution" "this" {
-  enabled = true
-  is_ipv6_enabled = true
-  comment = "for ${aws_s3_bucket.this.id}"
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "for ${aws_s3_bucket.this.id}"
   default_root_object = "index.html"
-  http_version = "http2and3"
-  price_class = "PriceClass_All"
+  http_version        = "http2and3"
+  price_class         = "PriceClass_All"
 
   origin {
-    domain_name = aws_s3_bucket.this.bucket_domain_name
-    origin_id = "s3-${aws_s3_bucket.this.id}"
+    domain_name              = aws_s3_bucket.this.bucket_regional_domain_name
+    origin_id                = "s3-${aws_s3_bucket.this.id}"
     origin_access_control_id = aws_cloudfront_origin_access_control.this.id
   }
 
   default_cache_behavior {
     target_origin_id = "s3-${aws_s3_bucket.this.id}"
 
-    allowed_methods = ["GET","HEAD"]
+    allowed_methods = ["GET", "HEAD"]
 
-    cached_methods = ["GET","HEAD"]
+    cached_methods = ["GET", "HEAD"]
 
-  viewer_protocol_policy = "redirect-to-https"
-  compress = true
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
 
-  cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
 
   }
   restrictions {
     geo_restriction {
       restriction_type = "whitelist"
-      locations = ["AU"]  # temp for playing around later
+      locations        = ["AU"] # temp for playing around later
     }
   }
   viewer_certificate {
